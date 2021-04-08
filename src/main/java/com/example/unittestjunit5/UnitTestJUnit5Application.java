4f661f4ff1a20da074c5d3f206bd3c52628053de
tasszz2k <tasszz2k@gmail.com>
2021-04-08 21:45:05 +0700

    first commit

---
+++ src/main/java/com/example/unittestjunit5/UnitTestJUnit5Application.java
@@ -0,0 +1,13 @@
+package com.example.unittestjunit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestJUnit5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestJUnit5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/unittestjunit5/UnitTestJUnit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unittestjunit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestJUnit5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
